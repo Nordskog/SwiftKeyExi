b8370f6ebb2d320ee831e0dba2d5043860913481
Roughy <mdawn700@gmail.com>
2017-10-02 13:16:28 +0900

    Log apk crc and module version, discard class cache on module update

--- app/src/main/java/com/mayulive/swiftkeyexi/LoadPackageHook.java
+++ app/src/main/java/com/mayulive/swiftkeyexi/LoadPackageHook.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.mayulive.swiftkeyexi.shared.SharedStyles;
 import com.mayulive.swiftkeyexi.xposed.ExiXposed;
 import com.mayulive.swiftkeyexi.xposed.Hooks;
+import com.mayulive.xposed.classhunter.ClassHunter;
 import com.mayulive.xposed.classhunter.ProfileCache;
 import com.mayulive.xposed.classhunter.ProfileServer;
 import com.mayulive.xposed.classhunter.packagetree.PackageTree;
@@ -19,10 +20,10 @@ public class LoadPackageHook implements IXposedHookLoadPackage
 	private static String LOGTAG = ExiModule.getLogTag(LoadPackageHook.class);
 
 	@Override
-    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable
-    {
+	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable
+	{
 		if ( !(lpparam.packageName.equals( ExiModule.SWIFTKEY_BETA_PACKAGE_NAME ) || lpparam.packageName.equals( ExiModule.SWIFTKEY_PACKAGE_NAME )) )
-            return;
+			return;
 
 		ExiXposed.HOOK_PACKAGE_NAME = lpparam.packageName;
 
@@ -30,13 +31,24 @@ public class LoadPackageHook implements IXposedHookLoadPackage
 		SharedStyles.setStyleContext(SharedStyles.StyleContext.HOOK);	//Defaults to app
 
 		XposedBridge.log(LOGTAG+", "+"Module loaded in "+lpparam.packageName);
-		Log.i(LOGTAG, "Module loaded in "+ExiXposed.HOOK_PACKAGE_NAME);
+		Log.i(
+				LOGTAG,
+				"Module loaded in "+ExiXposed.HOOK_PACKAGE_NAME+
+						", CRC: "+Integer.toHexString(ClassHunter.getApkCRC32(lpparam.appInfo.sourceDir))+
+						", Module version: "+BuildConfig.VERSION_NAME);
 
 		PackageTree classTree = new PackageTree(lpparam.appInfo.sourceDir, lpparam.classLoader);
+
+
 		ProfileCache.setSaveLocation(lpparam.appInfo.dataDir+"/files/EXI_CLASS_CACHE_"+ ExiXposed.HOOK_PACKAGE_NAME);
+
+		//Automatically reset class cache on update
+		ClassHunter.MODULE_SIGNATURE = BuildConfig.VERSION_CODE;
+
 		ProfileCache.loadCache();
 
 		Hooks.hookAll(classTree);
+
 		ProfileCache.saveCache();
 
 		XposedBridge.log(LOGTAG+", "+"Finished hooking work in"+lpparam.packageName);
