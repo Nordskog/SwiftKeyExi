90341e8d1e23c46bd7b810d8fa173f561f38166e
Roughy <mdawn700@gmail.com>
2019-02-07 01:28:17 +0100

    Added ability to set pref via SharedPrefProvider. Currently unused.

--- app/src/main/java/com/mayulive/swiftkeyexi/providers/SharedPreferencesProvider.java
+++ app/src/main/java/com/mayulive/swiftkeyexi/providers/SharedPreferencesProvider.java
@@ -8,13 +8,17 @@ import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Binder;
+import android.util.Log;
 
 import com.mayulive.swiftkeyexi.ExiModule;
 import com.mayulive.swiftkeyexi.settings.SettingsCommons;
 
+import java.util.Map;
+
 
 public class SharedPreferencesProvider extends ContentProvider
 {
+	private static String LOGTAG = ExiModule.getLogTag(SharedPreferencesProvider.class);
 
 	//
 	private static final String SHARED_PREFERENCES_PATH = "shared_prefs";
@@ -68,6 +72,64 @@ public class SharedPreferencesProvider extends ContentProvider
 	@Override
 	public Uri insert(Uri uri, ContentValues values)
 	{
+		if ( !ProviderSecurity.isAllowed( this.getContext(), Binder.getCallingUid() ) )
+			return null;
+
+		switch  (mSharedPreferencesUriMatcher.match(uri) )
+		{
+			case 0:	//Returns all shared prefs
+			{
+				try
+				{
+					SharedPreferences.Editor editor = SettingsCommons.getSharedPreferencesEditor(getContext());
+
+					for (Map.Entry<String, Object> entry : values.valueSet())
+					{
+						Object obj = entry.getValue();
+						if ( obj instanceof Long )
+						{
+							editor.putLong(entry.getKey(), (Long) obj);
+						}
+						else if ( obj instanceof Float )
+						{
+							editor.putFloat(entry.getKey(), (Float) obj);
+						}
+						else if ( obj instanceof Integer )
+						{
+							editor.putInt(entry.getKey(), (Integer) obj);
+						}
+						else if ( obj instanceof Boolean )
+						{
+							editor.putBoolean(entry.getKey(), (Boolean) obj);
+						}
+						else if ( obj instanceof String )
+						{
+							editor.putString(entry.getKey(), (String) obj);
+						}
+						else
+						{
+							Log.e(LOGTAG, "Unsupported type in SharedPref provider insert");
+						}
+					}
+
+					editor.apply();
+				}
+				catch ( Exception ex )
+				{
+					Log.e(LOGTAG, "Problem inserting shared pref");
+					ex.printStackTrace();
+				}
+
+
+			}
+
+			default:
+			{
+				//Nothing
+			}
+		}
+
+		// Always returns null
 		return null;
 	}
 
@@ -83,6 +145,39 @@ public class SharedPreferencesProvider extends ContentProvider
 		return -1;
 	}
 
+
+	public static <T> void setPreference( Context context, String key, T value )
+	{
+		ContentValues values = new ContentValues();
+
+		if ( value instanceof Long )
+		{
+			values.put(key, (Long) value);
+		}
+		else if ( value instanceof Float )
+		{
+			values.put(key, (Float) value);
+		}
+		else if ( value instanceof Integer )
+		{
+			values.put(key, (Integer) value);
+		}
+		else if ( value instanceof Boolean )
+		{
+			values.put(key, (Boolean) value);
+		}
+		else if ( value instanceof String )
+		{
+			values.put(key, (String) value);
+		}
+		else
+		{
+			Log.e(LOGTAG, "Unsupported type in SharedPref provider setPreference");
+		}
+
+		context.getContentResolver().insert(SHARED_PREFERENCES_PROVIDER_URI, values );
+	}
+
 	public static SharedPreferences getSharedPreferences(Context context)
 	{
 		Cursor cursor = context.getContentResolver().query(
@@ -93,9 +188,7 @@ public class SharedPreferencesProvider extends ContentProvider
 				null);                        	 // The sort order for the returned rows
 
 		SharedPreferencesCursor prefCursor = new SharedPreferencesCursor(cursor);
-		cursor.close();;
+		cursor.close();
 		return prefCursor;
-
-
 	}
 }
